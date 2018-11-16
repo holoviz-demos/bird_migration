88b2fde697344236a3b0f63eb9cf2d78d3896cf2
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2018-11-16 00:33:24 +0000

    Replaced script with notebook

--- panelserverextension.py
+++ panelserverextension.py
@@ -2,4 +2,4 @@ from subprocess import Popen
 
 def load_jupyter_server_extension(nbapp):
     """serve the app.ipynb directory with bokeh server"""
-    Popen(["panel", "serve", "app_no_temp.py", "--allow-websocket-origin=*"])
+    Popen(["panel", "serve", "app.ipynb", "--allow-websocket-origin=*"])
